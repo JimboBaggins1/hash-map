1585b909f9651833b5908565b573a391e0f08013
James Anderson <james.anderson98@outlook.com>
2024-07-11 13:02:24 +0200

    Create HashMap class

---
+++ src/HashMap.js
@@ -0,0 +1,9 @@
+class HashMap {
+    constructor (initialCapacity = 16, loadFactor = 0.75) {
+        this.loadFactor = loadFactor;
+        this.capacity = initialCapacity;
+        this.buckets = new Array(this.initialCapacity);
+    }
+
+    
+}
