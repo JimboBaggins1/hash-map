9541c0f3e53f6b119e6d4a44e2b87e9f3f1013da
James Anderson <james.anderson98@outlook.com>
2024-07-12 14:25:54 +0200

    Add hash() method

--- src/HashMap.js
+++ src/HashMap.js
@@ -1,9 +1,20 @@
-class HashMap {
-    constructor (initialCapacity = 16, loadFactor = 0.75) {
+import { Node } from "./Node";
+
+export class HashMap {
+    constructor(initialCapacity = 16, loadFactor = 0.75) {
         this.loadFactor = loadFactor;
         this.capacity = initialCapacity;
         this.buckets = new Array(this.initialCapacity);
     }
 
-    
+    hash(key) {
+        let hashCode = 0;
+           
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i++) {
+          hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
+        }
+     
+        return hashCode;
+    }
 }
