8a91139680283dc1beb6b2644d2a53c365f578ef
James Anderson <james.anderson98@outlook.com>
2024-07-12 14:57:31 +0200

    Add test cases

---
+++ src/index.js
@@ -0,0 +1,51 @@
+import { HashMap } from "./HashMap";
+
+let test = new HashMap();
+// console.log(test);
+// test.set('hey', 'there');
+// console.log(test.size);
+
+// test.set('hey', 'jude');
+// console.log(test.size);
+
+// test.set('hasdfasd', 'ther');
+// console.log(test.size);
+
+// test.set('hwerwey', 'the');
+// console.log(test.size);
+
+// // console.log(test);
+// // test.remove('hwerwey');
+// console.log(test);
+// console.log(test.size);
+
+// console.log(test.length)
+// // test.clear();
+// // console.log(test);
+// console.log(test.keys());
+// console.log(test.values());
+// console.log(test.entries());
+
+
+test.set('apple', 'red')
+test.set('banana', 'yellow')
+test.set('carrot', 'orange')
+test.set('dog', 'brown')
+test.set('elephant', 'gray')
+test.set('frog', 'green')
+test.set('grape', 'purple')
+test.set('hat', 'black')
+test.set('ice cream', 'white')
+test.set('jacket', 'blue')
+test.set('kite', 'pink')
+test.set('lion', 'golden')
+test.set('moon', 'silver')
+console.log(test.get('jackasdfet'))
+console.log(test.has('grasdfape'))
+test.remove('banasdna')
+console.log(test.length)
+console.log(test.keys())
+console.log(test.values())
+console.log(test.entries())
+// test.clear()
+console.log(test)
